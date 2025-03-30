8d3ca474ab92df76d896bb481f0e25ebae511418
Gulfam <mohd.gulfam@walmart.com>
2025-03-30 19:48:22 +0530

    n

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = location.hostname == 'localhost' ? 'http://localhost:5000': 'https://stocks-trends.onrender.com/';
-export const SERVER_URL_WS = location.hostname == 'localhost' ? 'ws://localhost:5000': 'wss://stocks-trends.onrender.com/';
+export const SERVER_URL = location.hostname == 'localhost' ? 'http://localhost:5000': 'https://stocks-trends.onrender.com';
+export const SERVER_URL_WS = location.hostname == 'localhost' ? 'ws://localhost:5000': 'wss://stocks-trends.onrender.com';

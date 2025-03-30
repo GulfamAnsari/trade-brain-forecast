7e7af23d323343b822a919ff30f650f4b0eed547
Gulfam <mohd.gulfam@walmart.com>
2025-03-30 19:21:32 +0530

    assd

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = location.hostname == 'localhost' ? 'http://localhost:5000': '/';
-export const SERVER_URL_WS = location.hostname == 'localhost' ? 'ws://localhost:5000': '/';
+export const SERVER_URL = location.hostname == 'localhost' ? 'http://localhost:5000': 'https://stock-trends.netlify.app';
+export const SERVER_URL_WS = location.hostname == 'localhost' ? 'ws://localhost:5000': 'wss://stock-trends.netlify.app';
